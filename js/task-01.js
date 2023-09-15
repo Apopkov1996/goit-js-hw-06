19d7b87d571693c3cedd2fc19c6ddf85addbb0af
Apopkov1996 <antonpopkov113@gmail.com>
2023-09-15 16:56:03 +0300

    update

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,9 @@
 const liElem = document.querySelectorAll('.item')
 console.log(`Number of categories: ${liElem.length}`);
 
+liElem.forEach(function (elem)) {
+    const headElem = liElem.querySelector('h2');
+    const itemElem = document.querySelector('');
+
+    console.log(headElem);
+}
